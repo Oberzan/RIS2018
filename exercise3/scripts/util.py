f42b021cc3554be9294f41cc544c6f8c997ee480
Matej <ematej.snuderl@gmail.com>
2018-03-30 18:31:26 +0200

    everything

--- exercise3/scripts/util.py
+++ exercise3/scripts/util.py
@@ -43,3 +43,7 @@ def nearest_goal(point, goals):
             max_dist = dist
 
     return (c_goal, max_dist)
+
+
+def point_distance(p1, p2):
+    return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**0.5
