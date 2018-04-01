75614329b1cdf9cc713f8276b7458a6c79513c6e
Matej <ematej.snuderl@gmail.com>
2018-04-01 19:28:08 +0200

    Rotating fixed

--- exercise3/scripts/moves.py
+++ exercise3/scripts/moves.py
@@ -9,12 +9,40 @@ from constants import ROTATING, DEFAULT, OBSERVING
 
 def rotate(velocity_publisher, speed, angle, state_publisher, step_angle=45, clockwise=True, sleep_duration=2):
     print("Started rotating")
-    vel_msg = Twist()
-
     # Converting from angles to radians
+    relative_angle = angle * 2 * math.pi / 360
+    current_angle = 0
+
+    num_stops = int(angle / step_angle)
+    print("Angle completed in {} steps.".format(num_stops))
+    string_message = String()
+
+    while current_angle < relative_angle:
+        # Send ROTATING state
+        string_message.data = ROTATING
+        state_publisher.publish(string_message)
+        print("Publishing state: {}".format(string_message.data))
+
+        # Rotate for step_angle
+        current_angle += rotate_inner(velocity_publisher, speed, step_angle, clockwise)
+
+        # Send OBSERVING state and sleeps for sleep_duration
+        string_message.data = OBSERVING
+        print("Publishing state: {}".format(string_message.data))
+        state_publisher.publish(string_message)
+        rospy.sleep(sleep_duration)
+
+    # Send DEFAULT state
+    string_message.data = DEFAULT
+    state_publisher.publish(string_message)
+    print("Publishing state: {}".format(string_message.data))
+    print("Finished rotating.")
+
+
+def rotate_inner(velocity_publisher, speed, angle, clockwise=True):
+    vel_msg = Twist()
     angular_speed = speed * 2 * math.pi / 360
     relative_angle = angle * 2 * math.pi / 360
-    relative_step_angle = step_angle * 2 * math.pi / 360
 
     # We wont use linear components
     vel_msg.linear.x = 0
@@ -22,41 +50,19 @@ def rotate(velocity_publisher, speed, angle, state_publisher, step_angle=45, clo
     vel_msg.linear.z = 0
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
-
-    actual_speed = -abs(angular_speed) if clockwise else abs(angular_speed)
+    vel_msg.angular.z = -abs(angular_speed) if clockwise else abs(angular_speed)
 
     # Setting the current time for distance calculus
     t0 = rospy.Time.now().to_sec()
     current_angle = 0
 
-    num_stops = int(angle / step_angle)
-    print("Angle completed in {} steps.".format(num_stops))
-
-    relative_angle_inner = current_angle + relative_step_angle
-    string_message = String()
-
     while current_angle < relative_angle:
-        vel_msg.angular.z = actual_speed
-        string_message.data = ROTATING
-        state_publisher.publish(string_message)
-        print("Publishing state: {}".format(string_message.data))
-
-        while current_angle < relative_angle_inner:
-            velocity_publisher.publish(vel_msg)
-            t1 = rospy.Time.now().to_sec()
-            current_angle = angular_speed * (t1 - t0)
-
-        vel_msg.angular.z = 0
         velocity_publisher.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        current_angle = angular_speed * (t1 - t0)
 
-        string_message.data = OBSERVING
-        print("Publishing state: {}".format(string_message.data))
-        state_publisher.publish(string_message)
-        rospy.sleep(sleep_duration)
-        relative_angle_inner += relative_step_angle
+    vel_msg.angular.z = 0
+    velocity_publisher.publish(vel_msg)
+    print("Rotated for: {}".format(angle))
 
-    string_message.data = DEFAULT
-    state_publisher.publish(string_message)
-    print("Publishing state: {}".format(string_message.data))
-
-    print("Finished rotating.")
+    return current_angle
