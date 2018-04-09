736e8fd4542618133f9f76ecfbe98adc8aa392d1
Dominik Sedmak <dominiksedmak@gmail.com>
2018-04-09 14:21:18 +0200

    Formatting 3.0

--- pcl_demos/src/cloud_viewer.cpp
+++ pcl_demos/src/cloud_viewer.cpp
@@ -4,18 +4,16 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
 
-
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 //...
-int
-main ()
+int main()
 {
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
   //... populate cloud
-  pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-  viewer.showCloud (cloud);
-  while (!viewer.wasStopped ())
+  pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+  viewer.showCloud(cloud);
+  while (!viewer.wasStopped())
   {
   }
   return 0;
