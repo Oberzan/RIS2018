46fd61be6ad24b211cf2cbe726007f5d6ae51570
Matej <ematej.snuderl@gmail.com>
2018-04-13 15:51:18 +0200

    Speedg

--- exercise4/scripts/data.py
+++ exercise4/scripts/data.py
@@ -5,9 +5,10 @@ class ClusterPoint():
         self.x = x
         self.y = y
         self.n = n
+        self.is_visited = False
 
     def __str__(self):
-        return "[x: {}, y: {}, n: {}]".format(self.x, self.y, self.n)
+        return "[x: {}, y: {}, n: {}, isVisited: {}]".format(self.x, self.y, self.n, self.is_visited)
 
     def __repr__(self):
         return self.__str__()
