99307af92293eb7f3f6cb0a975ec3ac9a8ab2e5c
Matej <ematej.snuderl@gmail.com>
2018-04-13 15:58:23 +0200

    Fix

--- exercise4/scripts/data.py
+++ exercise4/scripts/data.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 class ClusterPoint():
-    def __init__(self, x, y, n):
+    def __init__(self, x, y, n, is_visited=False):
         self.x = x
         self.y = y
         self.n = n
-        self.is_visited = False
+        self.is_visited = is_visited
 
     def __str__(self):
-        return "[x: {}, y: {}, n: {}, isVisited: {}]".format(self.x, self.y, self.n, self.is_visited)
+        return "[x: {}, y: {}, n: {}, is_visited: {}]".format(self.x, self.y, self.n, self.is_visited)
 
     def __repr__(self):
         return self.__str__()
@@ -16,4 +16,4 @@ class ClusterPoint():
     def move_center(self, p):
         new_x = (self.n * self.x + p.x) / (self.n + 1)
         new_y = (self.n * self.y + p.y) / (self.n + 1)
-        return ClusterPoint(new_x, new_y, self.n + 1)
+        return ClusterPoint(new_x, new_y, self.n + 1, p.is_visited)
