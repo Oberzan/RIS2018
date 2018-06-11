21e15f0f2c29bf5d4161cdd623c4caf54e459792
Matej <ematej.snuderl@gmail.com>
2018-06-12 01:57:31 +0200

    test hand

--- cryptomaster/scripts/cryptomaster.py
+++ cryptomaster/scripts/cryptomaster.py
@@ -341,14 +341,14 @@ def main():
     crypto_robot = CryptoMaster()
     crypto_robot.hand_manipulator.move_to_standby()
     # crypto_robot.extreme_mode_for_data_handler()
-    crypto_robot.run_robot()
+    # crypto_robot.run_robot()
 
-    # crypto_robot.hand_manipulator.grab_coin(0)
-    # crypto_robot.hand_manipulator.drop_coin()
-    # crypto_robot.hand_manipulator.grab_coin(1)
-    # crypto_robot.hand_manipulator.drop_coin()
-    # crypto_robot.hand_manipulator.grab_coin(2)
-    # crypto_robot.hand_manipulator.drop_coin()
+    crypto_robot.hand_manipulator.grab_coin(0)
+    crypto_robot.hand_manipulator.drop_coin()
+    crypto_robot.hand_manipulator.grab_coin(1)
+    crypto_robot.hand_manipulator.drop_coin()
+    crypto_robot.hand_manipulator.grab_coin(2)
+    crypto_robot.hand_manipulator.drop_coin()
 
 
 if __name__ == '__main__':
