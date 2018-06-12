65dcfb719a1dd8e46079eef0e70d3718cd9bdf9d
Matej <ematej.snuderl@gmail.com>
2018-06-12 03:15:55 +0200

    test

--- cryptomaster/scripts/trader.py
+++ cryptomaster/scripts/trader.py
@@ -31,49 +31,38 @@ class Trader(object):
     def _calculate_gains(self, color, group):
         data_points = list(group)
         merged_data = self.merge_data(data_points)
-        print("Color: ", color)
-        print("Merged data: ", merged_data)
-        print("Data points: ", data_points)
-
 
         points = merged_data.get('points', [])
         x = np.array([p[0] for p in points])
         y = np.array([p[1] for p in points])
         if len(points) > 2:
-            print("Calculating from multiple points")
-            print("Points: ", points)
             k, n = polyfit(x, y, 1)
-            print(k, n)
         elif len(points) == 2:
             p0 = points[0]
             p1 = points[1]
             k = (p1[1] - p0[1]) / (p1[0] - p0[0])
             n = p1[1] - (p1[0] * k)
         else:
-            print("Calculating from k and one point")
             k = merged_data.get('k')
             p = points[0]
             n = p[1] - (k * p[0])
 
         d5, d7, gains = self._calculate_gains_for_line(k, n)
-        ## x = np.append(x, np.array([5, 7]))
-        ## y = np.append(y, np.array([d5, d7]))
 
+        print("Color: ", color)
         print("k: ", k)
         print("n: ", n)
         print("5th day: ", d5)
         print("7th day: ", d7)
         print("Gains: ", gains)
-
-        ## plt.plot(x, y, '.')
-        ## plt.plot(x, n + (k * x), '-')
-        ## plt.show()
-        return {'color': color, "gains": gains}
+        print("Merged data: ", merged_data)
+        print("--------------\n\n")
+        return gains
 
 
     def get_job_gains(self, data_points):
-        print("DATA POINTS: ")
-        print(data_points)
+        print("DATA POINTS: ", data_points)
+        print("\n\n--------CALCULATING JOB GAINS--------------\n\n")
         data_points = sorted(data_points, key=lambda item: item.get_discrete_color())
         groups = groupby(data_points, lambda data_point: (data_point.get_discrete_color()))
         res = {}
